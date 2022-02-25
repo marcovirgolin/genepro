dd17486ef068c1c341d6afffacbaa4b931d4dd06
Marco <marco@Marcos-MacBook-Pro.local>
2022-02-25 23:55:12 +0100

    had forgotten to add __init__.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='genepro',
-    version='0.0.2',
+    version='0.0.3',
     author='Marco Virgolin',
     author_email='marco.virgolin@cwi.nl',
     url='https://github.com/marcovirgolin/genepro',
